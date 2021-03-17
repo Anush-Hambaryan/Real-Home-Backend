825bc06fe7b9740e9271a7527371deb3333033f8
Anush-Hambaryan <anushhambaryan@gmail.com>
2021-03-17 19:24:14 +0400

    first commit

--- realEstateApp/settings.py
+++ realEstateApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q68!(x9yll%u68p(0prq@r%-hs$-heq*rnxkfkg&kfbq4@0sf@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
